bb3c47a5745900ff9a253387bdc361f3f89d391e
root <nini.florea89@gmail.com>
2017-10-19 23:12:43 +0300

    Into to PDO

--- functions.php
+++ functions.php
@@ -1,6 +1,34 @@
 <?php
 
 
+
+
+function connectToDb()
+{
+	try {
+
+		return	$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'asdasd');
+
+	} catch(PDOException $e) {
+
+		die($e->getMessage());
+
+		}
+
+}
+
+function fetchAllTasks($pdo)
+{
+	$statement = $pdo->prepare('select * from todos');
+
+	$statement->execute();
+
+	return $tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+
+}
+
+
+
 function dd($data){
 
 	echo '<pre>';
