c098d9cd8aee0eeb577d0229b01e2139668bbf9b
voda <nini.florea89@gmail.com>
2017-10-29 17:50:45 +0200

    Hide Your Secret Passwords

--- database/Connection.php
+++ database/Connection.php
@@ -2,11 +2,18 @@
 
 class Connection
 {
-  public static function make()
+  public static function make($config)
   {
     try {
 
-      return	$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'asdasd');
+
+      return new PDO(
+            $config['connection'].';dbname='.$config['name'],
+            $config['username'],
+            $config['password'],
+            $config['options']
+
+        );
 
     } catch(PDOException $e) {
 
